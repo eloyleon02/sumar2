9cfb5a71828c3f036cfce97a56e1474d6389313f
eloyleon02 <150672234+eloyleon02@users.noreply.github.com>
2023-11-13 13:49:59 +0100

    Add files via upload

---
+++ GrBTestR2.R
@@ -0,0 +1,45 @@
+### ===========================================================
+### University Master in Statistics and Operational Research (MESIO)
+### Statistical Software: R and SAS 2021/2022
+### Test 1 with R (Group B): 05.11.2021
+### Name:
+### ===========================================================
+
+## Exercise 1
+## ===========
+
+# 1a
+
+
+
+
+# 1b
+
+
+
+
+# 1c
+
+
+
+
+# 1d
+
+
+
+
+## Exercise 2
+## ===========
+
+# 2a
+
+
+
+# 2b
+
+
+
+
+
+
+
